6eb1e81fadb75f75b6c2c6aa3b9835f552bd5d1a
ingridwarrior2901 <ingridguerrero@protonmail.com>
2019-02-19 15:46:59 +0100

    Added Dynamic Raycasting and SplineLine

---
+++ Source/WiccanGow/TrunkActor.cpp
@@ -0,0 +1,204 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TrunkActor.h"
+#include "Engine/EngineTypes.h"
+#include "DrawDebugHelpers.h"
+
+#pragma mark - Constructor
+
+ATrunkActor::ATrunkActor()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+    TruckLine = CreateDefaultSubobject<USplineComponent>(FName("TruckLine"));
+    TruckLine->SetupAttachment(RootComponent);
+}
+
+#pragma mark - LifeCycle
+
+void ATrunkActor::BeginPlay()
+{
+	Super::BeginPlay();
+    
+    LoadSplinePoints();
+    CreateSplineMesh();
+}
+
+
+void ATrunkActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+    
+    HandleRayCasting();
+}
+
+#pragma mark - Protected Methods
+
+void ATrunkActor::OnConstruction(const FTransform& Transform)
+{
+    if (bEditSplineLineOnEditMode)
+    {
+        CreateSplineMesh();
+    }
+}
+
+void ATrunkActor::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+    if (PropertyChangedEvent.Property != NULL &&
+        (PropertyChangedEvent.Property->GetFName() == FName(TEXT("NumberOfSplinePoints"))))
+    {
+        TruckLine->ClearSplinePoints();
+        LoadSplinePoints();
+        
+        ReloadSplineMesh();
+    }
+    
+    Super::PostEditChangeProperty(PropertyChangedEvent);
+}
+
+
+#pragma mark - Private Methods
+
+void ATrunkActor::HandleRayCasting()
+{
+    FHitResult OutHit;
+    FVector Start = SpotLightActor->GetActorLocation();
+    
+    FVector ForwardVector = SpotLightActor->GetActorForwardVector();
+    FVector End = ((ForwardVector * 1000.f) + Start);
+    FCollisionQueryParams CollisionParams;
+    
+    DrawDebugLine(GetWorld(), Start, End, FColor::Green, false, 1, 0, 1);
+    
+    if(GetWorld()->LineTraceSingleByChannel(OutHit, Start, End, ECC_Visibility, CollisionParams))
+    {
+        UPrimitiveComponent* HitPrimitiveLocation = OutHit.GetComponent();
+        USplineMeshComponent* SplineMeshComponent = Cast<USplineMeshComponent>(HitPrimitiveLocation);
+        
+        if (SplineMeshComponent)
+        {
+            int OutIndex;
+            
+            if (SplineMeshComponents.Find(SplineMeshComponent, OutIndex))
+            {
+                //TODO: Consider to refactor this ->
+                int positionDistance = YCurveDistancePoints;
+                
+                int LastPointX;
+                bool bCalculateCurve = false;
+                
+                if (!bIsRayCastingDisplaying)
+                {
+                    for (int32 i = OutIndex; i < TruckLine->GetNumberOfSplinePoints(); i++)
+                    {
+                        FVector lastPoint = TruckLine->GetLocationAtSplinePoint(i, ESplineCoordinateSpace::Type::Local);
+                        
+                        if (i == OutIndex)
+                        {
+                            LastPointX = lastPoint.X;
+                        }
+                        else if (i > OutIndex && i < TruckLine->GetNumberOfSplinePoints() && !bCalculateCurve)
+                        {
+                            LastPointX = LastPointX + MaxCurveHeightDistance;
+                            bCalculateCurve = true;
+                        }
+                        
+                        FVector UpdatedVector = FVector(LastPointX, positionDistance, 0.0f);
+                        positionDistance+= YCurveDistancePoints;
+                        
+                        TruckLine->SetLocationAtSplinePoint(i, UpdatedVector, ESplineCoordinateSpace::Type::Local, true);
+                    }
+                    
+                    bIsRayCastingDisplaying = true;
+                    ReloadSplineMesh();
+                }
+            }
+        }
+    }
+}
+
+
+void ATrunkActor::ReloadSplineMesh()
+{
+    //TODO: this is a workaround to dynamically show the spline meshes, don't ask me why! it's unreal fucking bug!
+    FTimerHandle timer;
+    GetWorldTimerManager().SetTimer(timer, this, &ATrunkActor::CreateSplineMesh, CreateSplineMeshDelay);
+}
+
+void ATrunkActor::ClearUSplineMeshComponents()
+{
+    if (SplineMeshComponents.Num() > 0)
+    {
+        for (int32 i = 0; i < SplineMeshComponents.Num(); i++)
+        {
+            if (SplineMeshComponents[i])
+            {
+                SplineMeshComponents[i]->DetachFromParent();
+                SplineMeshComponents[i]->DestroyComponent();
+            }
+        }
+        SplineMeshComponents.Empty();
+    }
+}
+
+#pragma mark - Public Methods
+
+void ATrunkActor::LoadSplinePoints()
+{
+    if (TruckLine)
+    {
+        TArray <FVector> Points;
+        
+        int32 CurrentPoint = StartPoint;
+        
+        for (int32 i = 0; i < NumberOfSplinePoints; i++)
+        {
+            FVector Point = FVector(CurrentPoint, 0.0f, 0.0f);
+            Points.Add(Point);
+            CurrentPoint += ZDistanceBetweenPoints;
+        }
+        
+        TruckLine->SetSplinePoints(Points, ESplineCoordinateSpace::Type::Local, true);
+    }
+}
+
+void ATrunkActor::CreateSplineMesh()
+{
+    FVector LocStart, TanStart, LocEnd, TanEnd;
+    
+    ClearUSplineMeshComponents();
+    
+    int32 NumberOfPoints = TruckLine->GetNumberOfSplinePoints();
+    
+    if (NumberOfPoints < 2)
+    {
+        UE_LOG(LogTemp, Display, TEXT("Can't create the spline mesh, need more points"));
+        return;
+    }
+    
+    for (int32 i = 0; i < (NumberOfPoints - 1); i++)
+    {
+        USplineMeshComponent* SplineMeshComponent = NewObject<USplineMeshComponent>(this, USplineMeshComponent::StaticClass());
+        SplineMeshComponent->RegisterComponent();
+        SplineMeshComponent->CreationMethod = EComponentCreationMethod::UserConstructionScript;
+        
+        TruckLine->GetLocationAndTangentAtSplinePoint(i, LocStart, TanStart, ESplineCoordinateSpace::Local);
+        TruckLine->GetLocationAndTangentAtSplinePoint(i + 1, LocEnd, TanEnd, ESplineCoordinateSpace::Local);
+        
+        SplineMeshComponent->SetStartScale(FVector2D(SplineLineScale, SplineLineScale));
+        SplineMeshComponent->SetEndScale(FVector2D(SplineLineScale, SplineLineScale));
+        
+        SplineMeshComponent->SetMobility(EComponentMobility::Type::Movable);
+        SplineMeshComponent->SetForwardAxis(ESplineMeshAxis::X);
+        
+        SplineMeshComponent->SetStaticMesh(SplineStaticMesh);
+        
+        SplineMeshComponent->SetCollisionObjectType(ECollisionChannel::ECC_WorldStatic);
+        SplineMeshComponent->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+        SplineMeshComponent->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Block);
+        SplineMeshComponent->SetStartAndEnd(LocStart, TanStart, LocEnd, TanEnd);
+        SplineMeshComponent->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepRelativeTransform);
+        
+        SplineMeshComponents.Add(SplineMeshComponent);
+    }
+}
---
+++ Source/WiccanGow/TrunkActor.h
@@ -0,0 +1,78 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SplineComponent.h"
+#include "Components/SplineMeshComponent.h"
+#include "GameFramework/Actor.h"
+#include "Engine/SpotLight.h"
+#include "TrunkActor.generated.h"
+
+UCLASS()
+class WICCANGOW_API ATrunkActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:
+	ATrunkActor();
+
+protected:
+
+	virtual void BeginPlay() override;
+    
+    virtual void OnConstruction(const FTransform &Transform) override;
+    
+    virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+
+
+public:
+	virtual void Tick(float DeltaTime) override;
+    void CreateSplineMesh();
+    void LoadSplinePoints();
+    
+public:
+    
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Spline")
+    USplineComponent* TruckLine;
+    
+    UPROPERTY(EditAnywhere, Category = "SplineLine Mesh")
+    UStaticMesh* SplineStaticMesh;
+    
+    UPROPERTY(EditAnywhere, Category = "SplineLine Mesh")
+    bool bEditSplineLineOnEditMode;
+    
+    UPROPERTY(EditAnywhere, Category = "SplineLine Mesh")
+    int32 NumberOfSplinePoints = 5;
+    
+    UPROPERTY(EditAnywhere, Category = "SplineLine Mesh")
+    int32 ZDistanceBetweenPoints = 100;
+    
+    UPROPERTY(EditAnywhere, Category = "SplineLine Mesh")
+    int32 YCurveDistancePoints = 200;
+    
+    UPROPERTY(EditAnywhere, Category = "SplineLine Mesh")
+    int32 MaxCurveHeightDistance = 50;
+    
+    UPROPERTY(EditAnywhere, Category = "SplineLine Mesh")
+    float SplineLineScale = 0.2;
+    
+    UPROPERTY(VisibleAnywhere, Category = "SplineLine Mesh")
+    TArray<USplineMeshComponent*> SplineMeshComponents;
+    
+    UPROPERTY(EditAnywhere, Category = "SplineLine Mesh")
+    ASpotLight* SpotLightActor;
+    
+    const int32 StartPoint = 100.0f;
+    const float CreateSplineMeshDelay = 0.2f;
+  
+    int CurrentRayCastIndex;
+    
+    bool bIsRayCastingDisplaying = false;
+    
+private:
+    
+    void HandleRayCasting();
+    void ReloadSplineMesh();
+    void ClearUSplineMeshComponents();
+};
